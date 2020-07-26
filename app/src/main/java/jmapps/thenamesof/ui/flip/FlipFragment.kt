65c5c1a2f430db0bec648c99905b285428230fd5
jmapps <jmappsmuslim@gmail.com>
2020-07-26 22:05:38 +0300

    Without viewmodel

--- app/src/main/java/jmapps/thenamesof/ui/flip/FlipFragment.kt
+++ app/src/main/java/jmapps/thenamesof/ui/flip/FlipFragment.kt
@@ -4,16 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import jmapps.thenamesof.R
-import jmapps.thenamesof.databinding.FragmentContentBinding
 import jmapps.thenamesof.databinding.FragmentFlipBinding
-import jmapps.thenamesof.ui.content.ContentViewModel
 
 class FlipFragment : Fragment() {
 
