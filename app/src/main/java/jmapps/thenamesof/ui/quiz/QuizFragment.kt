7ea2ddd02d522c319fcacdfdbc8611ead59dddcc
jmapps <jmappsmuslim@gmail.com>
2020-08-03 15:58:20 +0300

    Remove view model

--- app/src/main/java/jmapps/thenamesof/ui/quiz/QuizFragment.kt
+++ app/src/main/java/jmapps/thenamesof/ui/quiz/QuizFragment.kt
@@ -4,16 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import jmapps.thenamesof.R
-import jmapps.thenamesof.databinding.FragmentMainBinding
 import jmapps.thenamesof.databinding.FragmentQuizBinding
-import jmapps.thenamesof.ui.main.MainViewModel
 
 class QuizFragment : Fragment() {
 
