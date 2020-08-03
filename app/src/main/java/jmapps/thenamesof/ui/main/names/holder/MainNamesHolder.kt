b563b9ccab14d22f2ec90802f53636e83e21c9c8
jmapps <jmappsmuslim@gmail.com>
2020-08-03 17:51:24 +0300

    Changed id's

--- app/src/main/java/jmapps/thenamesof/ui/main/names/holder/MainNamesHolder.kt
+++ app/src/main/java/jmapps/thenamesof/ui/main/names/holder/MainNamesHolder.kt
@@ -11,7 +11,7 @@ class MainNamesHolder(namesView: View) : RecyclerView.ViewHolder(namesView) {
 
     val tvMainArabicName: TextView = namesView.findViewById(R.id.tvMainArabicName)
     val tvMainTranscriptionName: TextView = namesView.findViewById(R.id.tvMainTranscriptionName)
-    val tvMainTranslateName: TextView = namesView.findViewById(R.id.tvMainTranslateName)
+    val tvMainTranslateName: TextView = namesView.findViewById(R.id.tvMainTranslationName)
     val ivPlayName: ImageView = namesView.findViewById(R.id.ivPlayName)
 
     fun findMainNamesItemClick(onItemMainNameClick: MainNamesAdapter.OnItemMainNameClick, mainNameId: Int) {
