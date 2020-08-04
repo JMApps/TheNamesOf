3f76cad81aa5a449cf062ae27fcdb9a5a7e45bb9
jmapps <jmappsmuslim@gmail.com>
2020-08-04 16:01:24 +0300

    Removed view model flip

--- app/src/main/java/jmapps/thenamesof/ui/flip/FlipFragment.kt
+++ app/src/main/java/jmapps/thenamesof/ui/flip/FlipFragment.kt
@@ -6,23 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import jmapps.thenamesof.R
 import jmapps.thenamesof.databinding.FragmentFlipBinding
 
 class FlipFragment : Fragment() {
 
-    private lateinit var flipViewModel: FlipViewModel
     private lateinit var binding: FragmentFlipBinding
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        flipViewModel = ViewModelProvider(this).get(FlipViewModel::class.java)
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_flip, container, false)
-
-        flipViewModel.text.observe(viewLifecycleOwner, Observer {
-
-        })
 
         return binding.root
     }
