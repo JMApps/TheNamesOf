80651922a1864f5205ae9986ecac7d027aa4fb95
jmapps <jmappsmuslim@gmail.com>
2020-09-01 17:41:52 +0300

    Without AppBarConfiguration

--- app/src/main/kotlin/jmapps/MainActivity.kt
+++ app/src/main/kotlin/jmapps/MainActivity.kt
@@ -5,13 +5,11 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import androidx.preference.PreferenceManager
 import com.google.android.material.navigation.NavigationView
 import jmapps.thenamesof.R
@@ -25,7 +23,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     OtherContract.OtherView {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var appBarConfiguration: AppBarConfiguration
 
     private lateinit var preferences: SharedPreferences
     private lateinit var editor: SharedPreferences.Editor
@@ -38,27 +35,32 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setSupportActionBar(binding.appBarMain.toolbar)
 
+        otherPresenterImpl = OtherPresenterImpl(this, this)
+
         preferences = PreferenceManager.getDefaultSharedPreferences(this)
         editor = preferences.edit()
 
-        otherPresenterImpl = OtherPresenterImpl(this, this)
-
-        val navController = findNavController(R.id.main_fragment_container)
-
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_book_content,
-                R.id.nav_names_content,
-                R.id.nav_all_names,
-                R.id.nav_name_flip_card,
-                R.id.nav_name_input_card,
-                R.id.nav_quiz
-            ), binding.drawerLayout
+        val toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            binding.appBarMain.toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
         )
 
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        binding.navigationView.setupWithNavController(navController)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
         binding.navigationView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -66,13 +68,22 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return true
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.main_fragment_container)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
-
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.main_fragment_container)
         when (item.itemId) {
+
+            R.id.nav_book_content -> navController.navigate(R.id.nav_book_content)
+
+            R.id.nav_names_content -> navController.navigate(R.id.nav_names_content)
+
+            R.id.nav_all_names -> navController.navigate(R.id.nav_all_names)
+
+            R.id.nav_name_flip_card -> navController.navigate(R.id.nav_name_flip_card)
+
+            R.id.nav_name_input_card -> navController.navigate(R.id.nav_name_input_card)
+
+            R.id.nav_quiz -> navController.navigate(R.id.nav_quiz)
+
             R.id.nav_download_audio -> otherPresenterImpl.downloadAllAudios()
 
             R.id.nav_donate -> otherPresenterImpl.donateProject()
@@ -82,7 +93,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             R.id.nav_rate -> otherPresenterImpl.rateApp()
 
             R.id.nav_share -> otherPresenterImpl.shareAppLink()
-            }
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
         return true
     }
 
